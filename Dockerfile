4350dfbb70d9b3293bd12f595afed236e45e783f
Little Human <hey@moojok.xyz>
2023-01-22 16:48:49 +0700

    Sometime regex is not your friend

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM frolvlad/alpine-gcc
-RUN apk add --no-cache pcre-dev libc-dev
+RUN apk add --no-cache libc-dev
 WORKDIR /app
 COPY alt-fast.c /app/main.c
-RUN gcc -o server main.c -lpcre -lpthread
+RUN gcc -o server main.c -lpthread
 CMD ["/app/server"]
--- alt-fast.c
+++ alt-fast.c
@@ -1,5 +1,4 @@
 #include <netinet/in.h>
-#include <pcre.h>
 #include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
@@ -13,13 +12,13 @@
 #define NUM_THREADS 8
 #define PORT 3030
 
-struct timeval previous_time;
-double rps;
+int rps;
+int requests = 0;
 pthread_mutex_t rps_mutex;
 pthread_t rps_thread;
+struct timeval start_time;
 
 int my_socket;
-
 pthread_t threadpool[NUM_THREADS];
 
 int handle_request(int client_socket) {
@@ -27,14 +26,9 @@ int handle_request(int client_socket) {
   int valread = read(client_socket, buffer, 1024);
   char *response = "{\"message\": \"Hello world\"}";
 
-  // Use regular expression library to check if the url is "/hello"
-  pcre *re;
-  const char *error;
-  int erroffset;
-  re = pcre_compile("GET /hello", 0, &error, &erroffset, NULL);
-  int rc = pcre_exec(re, NULL, buffer, strlen(buffer), 0, 0, NULL, 0);
-  pcre_free(re);
-  if (rc >= 0) {
+  // Use strstr() to check if the url is "/hello"
+  char *match = strstr(buffer, "GET /hello");
+  if (match != NULL) {
     char *http_response = malloc(strlen(response) +
                                  strlen("HTTP/1.1 200 OK\nContent-Type: "
                                         "application/json\nContent-Length: ") +
@@ -45,6 +39,7 @@ int handle_request(int client_socket) {
             (int)strlen(response), response);
     write(client_socket, http_response, strlen(http_response));
     free(http_response);
+    requests++;
     return 1;
   }
   return 0;
@@ -73,8 +68,18 @@ void *calculate_rps(void *arg) {
   while (1) {
     sleep(1);
     pthread_mutex_lock(&rps_mutex);
-    printf("RPS: %.2f\n", rps);
-    rps = 0;
+    if (requests > 0) {
+      struct timeval current_time;
+      gettimeofday(&current_time, NULL);
+      double time_elapsed =
+          (current_time.tv_sec - start_time.tv_sec) +
+          (current_time.tv_usec - start_time.tv_usec) / 1000000.0;
+      double rps_ms = requests / time_elapsed;
+      printf("RPS: %.2f ms\n", rps_ms);
+      rps = 0;
+      requests = 0;
+      start_time = current_time;
+    }
     pthread_mutex_unlock(&rps_mutex);
   }
   return NULL;
@@ -88,6 +93,7 @@ void handle_sigint(int sig) {
 int main(int argc, char *argv[]) {
   signal(SIGINT, handle_sigint);
   pthread_mutex_init(&rps_mutex, NULL);
+  gettimeofday(&start_time, NULL);
 
   struct sockaddr_in server_address;
   my_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -106,9 +112,11 @@ int main(int argc, char *argv[]) {
 
   pthread_create(&rps_thread, NULL, calculate_rps, NULL);
 
-  while (1) {
-    sleep(1);
+  for (int i = 0; i < NUM_THREADS; i++) {
+    pthread_join(threadpool[i], NULL);
   }
+
+  pthread_join(rps_thread, NULL);
 
   return 0;
 }
